b798d7ffae56117dec41bc59430ddbac44169743
Guruvel13 <letters2guruvel@gmail.com>
2025-07-16 10:04:52 +0530

    Initial commit

---
+++ src/test/java/com/example/ShoppingChatBot/ShoppingChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
